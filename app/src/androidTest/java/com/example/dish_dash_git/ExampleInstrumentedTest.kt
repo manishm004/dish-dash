9425c9fe0662f7145ecf9e43a19473832f32c90f
Manish M <manish.muru07@gmail.com>
2026-06-05 11:13:23 +0530

    Changed to 12345

--- app/src/androidTest/java/com/example/dish_dash_git/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/dish_dash_git/ExampleInstrumentedTest.kt
@@ -18,7 +18,7 @@ class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext3 = InstrumentationRegistry.getInstrumentation().targetContext
+        val appContextdummy = InstrumentationRegistry.getInstrumentation().targetContext
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.dish_dash_git", appContext.packageName)
     }
